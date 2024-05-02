29b9c5e551032c0a98061537fe34a22896fcb76c
GabrielPico24 <kevin.pico12@hotmail.com>
2024-05-01 20:46:09 -0500

    GABRIEL-PICO 20:45

--- lib/search_trips_page.dart
+++ lib/search_trips_page.dart
@@ -11,47 +11,121 @@ class SearchTripsPage extends StatelessWidget {
         backgroundColor: const Color(0xFF4268D3),
       ),
       backgroundColor: const Color(0xFF4268D3),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.white, // Color de fondo del contenedor de búsqueda
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white, // Color de fondo del contenedor de búsqueda
+                  ),
+                  child: Row(
+                    children: [
+                      const Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Knucles Mount Range',
+                            contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.search),
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: const Text('Gabriel Pico'),
+                              duration: const Duration(seconds: 2), // Duración del SnackBar
+                              action: SnackBarAction(
+                                label: 'Cerrar',
+                                onPressed: () {
+                                  // Acción cuando se presiona el botón de cerrar
+                                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                                },
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              child: Row(
+              const SizedBox(height: 20), 
+              Stack(
                 children: [
-                  const Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Knucles Mount Range',
-                        contentPadding: EdgeInsets.symmetric(horizontal: 16),
-                        border: InputBorder.none,
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                    child: Image.asset(
+                      'assets/images/cascade.jpg', 
+                      fit: BoxFit.cover, 
+                      height: 500, 
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    child: Container(
+                      height: 60, // Altura del degradado
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.transparent,
+                            Colors.black.withOpacity(0.5), // Color de los rectángulos con opacidad
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                  IconButton(
-                    icon: const Icon(Icons.search),
-                    onPressed: () {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: const Text('Gabriel Pico'),
-                          duration: const Duration(seconds: 2), // Duración del SnackBar
-                          action: SnackBarAction(
-                            label: 'Cerrar',
-                            onPressed: () {
-                              // Acción cuando se presiona el botón de cerrar
-                              ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                            },
-                          ),
-                        ),
-                      );
-                    },
-                  ),
                 ],
               ),
+            ],
+          ),
+          Positioned(
+            top: 0,
+            right: 0,
+            bottom: 0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Icon(Icons.add),
+                  style: ElevatedButton.styleFrom(
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(16),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Icon(Icons.remove),
+                  style: ElevatedButton.styleFrom(
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(16),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Icon(Icons.refresh),
+                  style: ElevatedButton.styleFrom(
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(16),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
